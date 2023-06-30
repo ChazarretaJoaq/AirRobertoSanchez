78dafba9a679dd45914b9f900a1a7dce5cb9eee7
Joaquin <gustavo.chazarreta10@gmail.com>
2023-06-30 15:20:16 -0300

    Initial Commit

---
+++ RobertoSanchez/RobertoSanchezApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobertoSanchezApp.swift
+//  RobertoSanchez
+//
+//  Created by gustavo joaquin chazarreta on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RobertoSanchezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
